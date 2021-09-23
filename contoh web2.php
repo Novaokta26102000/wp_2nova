44f2c084d64a28a3ca2e8e4e8bc270d745436879
Novaokta26102000 <oktaviawannova26102000>
2021-09-23 10:49:04 +0700

    first commit

---
+++ contoh web2.php
@@ -0,0 +1,22 @@
+<?php
+{
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama Saya : Nova Oktaviawan 
+		
+		nim saya: 12200756<?php
+
+		
+		jurusan saya : Sistem informasi
+		
+		Saya kuliah di : BSI 
+
+		saya tinggal di daerah : Bogor 
+		
+		Hobby Saya Adalah : Jalan-jalan";
+
+	}
+
+		
+
+}
